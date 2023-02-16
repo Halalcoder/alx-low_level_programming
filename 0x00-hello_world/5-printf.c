2483b2423c800a14f3c7ce9d730d5c80bebcce8d
Halalcoder <muadhadesope@student.oauife.edu.ng>
2023-02-16 12:21:55 -0800

    Hello, printf

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * main - write a c program with printf followed by a newline
+ *
+ * Return: 0
+ */
+
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+
+	Return (0);
+}
+
