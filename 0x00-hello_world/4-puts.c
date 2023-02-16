59dffc2bde26441cad6a2b1be95bbb8e90e5cfe2
Halalcoder <muadhadesope@student.oauife.edu.ng>
2023-02-16 11:51:34 -0800

    Hello, puts

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+ * main - Write a C program that prints exactly 
+ * Programming is like building a multilingual puzzle,
+ * followed by a new line.
+ * Return: 0
+ */
+
+int main(void)
+{
+	puts("Programming is like building a multilingual puzzl.\n");
+
+	return (0);
+}
