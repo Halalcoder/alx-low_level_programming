9a068cf6d5305a2ec483bc0f07b0f4285c9bb3d2
Halalcoder <muadhadesope@student.oauife.edu.ng>
2023-02-17 10:53:55 -0800

    Positive anything is better than negative nothing

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -4,8 +4,6 @@
 
 /**
  * main - Determines if a number is positive,negative or zero
- * Description: A program that checks if
- * a number is positive, negative or zero
  *
  * Result: Always 0 (success)
  */
