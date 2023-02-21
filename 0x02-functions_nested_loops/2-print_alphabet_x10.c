5d71c6e0f4da1b7b22101acd8efedad564301952
Halalcoder <muadhadesope@student.oauife.edu.ng>
2023-02-21 12:50:54 -0800

    10 x alphabet

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * main - a function that orints ten times the alphabets
+ *
+ * print_alphabet_x10 - print alphabets
+ * Return: Always 0
+ */
+
+void print_alphabet_x10(void)
+{
+	int i, j;
+
+	for (i = 0; i < 10; i++)
+	{
+		for (j = 'a'; j <= 'z'; j++)
+			_putchar(j);
+		putchar('\n');
+
+	}
+}
