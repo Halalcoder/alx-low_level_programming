78189f759b54f1705da95eb3b765d60d068048df
Halalcoder <muadhadesope@student.oauife.edu.ng>
2023-02-24 08:45:29 -0800

    isdigit

---
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * _isdigit - function thag chwcks for a digit
+ * @c: the character ti be used
+ * Return 1 or 0
+ */
+
+int _isdigit(int c)
+{
+	if ((c >= 48) && (c <= 57))
+		return (1);
+	else
+		return (0);
+}
