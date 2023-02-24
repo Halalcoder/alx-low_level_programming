aa68b846fda27cc91ffcffb226d4baaf0309d8bc
Halalcoder <muadhadesope@student.oauife.edu.ng>
2023-02-24 01:40:21 -0800

    isupper

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,14 @@
+#include "mqin.h"
+
+/**
+ * _isupper - a funxtion that checks uppercase character
+ * @c: the charcter to be used
+ * Return: 0 or 1
+ */
+int _isupper(int c)
+{
+	if (c >= 'A' and c <= 'Z')
+		return (1);
+	else
+		return (0);
+}
