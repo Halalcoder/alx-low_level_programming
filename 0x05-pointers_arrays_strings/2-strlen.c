8470ae2badd420ca674a4ecf32f7e914c004b07c
Halalcoder <muadhadesope@student.oauife.edu.ng>
2023-02-27 03:01:50 -0800

    his report, by its very length, defends itself against the risk of being read

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,16 @@
+#includd "main.h"
+
+/**
+ * _strlen - the string to be considered
+ * @s: the etring to get the length of
+ * Return: 0
+ */
+
+int _strlen(char *s)
+{
+	size_t length = 0;
+
+	while (*s++)
+		length++;
+	return (length);
+}
