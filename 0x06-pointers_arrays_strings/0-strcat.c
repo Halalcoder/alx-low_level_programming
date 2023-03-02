1502b6178a801bb00ce2da17a3dd1920376c4661
Halalcoder <muadhadesope@student.oauife.edu.ng>
2023-03-02 03:45:49 -0800

    strcat

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ * _strcat - a functiom that concatenates twi strings
+ * @src: the source
+ * @dest: the destination
+ * Return: the length of the string
+ */
+
+char *_strcat(char *dest, char *src)
+{
+	int i;
+	int j;
+
+	i = 0;
+	while (dest[i] != '\0')
+		i++;
+	j = 0;
+	while (dest[j] != '\0')
+	{
+		dest[i] = src[j];
+		i++;
+		j++;
+	}
+	dest[i] = '\0';
+	return (dest);
+}
