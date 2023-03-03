495d622af3e7b1c9a1f188e3c7a8c7a30e509833
Halalcoder <muadhadesope@student.oauife.edu.ng>
2023-03-02 20:47:45 -0800

    Always look up

--- 0x06-pointers_arrays_strings/5-string_toupper.c
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -5,15 +5,15 @@
  * @n: pointer
  * Return: n
  */
-char *string_toupper(char *)
+char *string_toupper(char *n)
 {
 	int i;
 
 	i = 0;
 	while (n[i] != '\0')
 	{
-		if (n[i] !=  'a' && n[i] != 'z')
-			n[i] = n[i] - 33;
+		if (n[i] >=  'a' && n[i] <= 'z')
+			n[i] = n[i] - 32;
 		i++;
 	}
 	return (n);
