471a6f0d86435abe5ff7812d1ff281bf2ab8882e
Halalcoder <muadhadesope@student.oauife.edu.ng>
2023-03-15 01:26:57 -0700

    He who is not courageous enough to take risks will accomplish nothing in life

---
+++ 0x0B-malloc_free/2-str_concat.c
@@ -0,0 +1,41 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * str_concat - get ends of input and add it together for size
+ * @s1: the first input
+ * @s2: the second input
+ * Return: concat of s1 and s2
+ */
+char *str_concat(char *s1, char *s2)
+{
+	char *conc;
+	int i, c;
+
+	if (s1 == NULL)
+		s1 = "";
+	if (s2 == NULL)
+		s2 = "";
+	i = c = 0;
+	while (s1[i] != '\0')
+		i++;
+	while (s1[c] != '\0')
+		c++;
+	conc = malloc(sizeof(char) * (i + c + 1));
+
+	if (conc == NULL)
+		return (NULL);
+	i = c = 0;
+	while (s1[i] != '\0')
+	{
+		conc[i] = s1[i];
+		i++;
+	}
+	while (s2[c] != '\0')
+	{
+		conc[i] = s2[c];
+		i++, c++;
+	}
+	conc[i] = '\0';
+	return (conc);
+}
